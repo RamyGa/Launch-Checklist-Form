47b0f19a4ac1892618399ef34b7386dcf77a9a5f
Ramyga <research@ronolabs.com>
2020-12-14 14:50:18 -0600

    added id to index.html elements & added validation to script.js

--- script.js
+++ script.js
@@ -11,3 +11,34 @@
 </ol>
 <img src="${}">
 */
+
+
+
+
+window.addEventListener("load", function () {
+   let pilotName = document.getElementById("pilotName");
+   let copilotName = document.getElementById("copilotName");
+   let fuelLevel = document.getElementById("fuelLevel");
+   let cargoMass =document.getElementById("cargoMass");
+   let form = document.querySelector("form");
+
+   let fuelAsInt = Number(fuelLevel.value);
+   let cargoMassAsInt = Number(cargoMass.value);
+
+   console.log(typeof(x));
+   form.addEventListener("submit", function (event) {
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         
+         alert("All fields are required");
+         event.preventDefault();
+      }
+
+      else if (isNaN(pilotName.value) === false || isNaN(copilotName.value) === false || isNaN(fuelAsInt) || isNaN(cargoMassAsInt)) {
+         alert("Make sure to enter valid information for each field!");
+         event.preventDefault();
+      }
+  
+
+   });
+
+});
